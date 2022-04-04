d4afd94236b8a48ef1b12a405aec76b06720a723
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-04 21:26:28 +0400

    fix: настроить валидацию поля about с помощью Joi

--- routes/users.js
+++ routes/users.js
@@ -20,8 +20,8 @@ router.get('/:userId', celebrate({
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
+    name: Joi.string().min(2).max(30).required(),
   }),
 }), updateUser);
 

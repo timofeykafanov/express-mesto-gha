76939434ec0546b2449a9abcecdee7ae6a2d2137
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-04 21:42:25 +0400

    refactor: добавить валидацию ссылки в схеме карточки

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = mongoose.Schema({
   name: {
@@ -10,6 +11,10 @@ const cardSchema = mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (link) => validator.isURL(link, { protocols: ['http', 'https'], require_tld: true, require_protocol: true }),
+      message: 'Неправильный формат ссылки',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,

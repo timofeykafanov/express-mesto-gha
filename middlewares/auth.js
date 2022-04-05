d168eaa09e6eaa77feea050d6e17247b5b7ff152
Timofey Kafanov <tim.kafanov@gmail.com>
2022-04-05 13:02:30 +0400

    refactor: сделать обработку ошибки через класс

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const handleAuthError = (res) => {
-  res
-    .status(401)
-    .send({ message: 'Необходима авторизация' });
-};
+const AuthError = require('../errors/AuthError');
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
@@ -12,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    throw new AuthError('Необходима авторизация');
   }
 
   const token = extractBearerToken(authorization);
@@ -21,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return handleAuthError(res);
+    throw new AuthError('Необходима авторизация');
   }
 
   req.user = payload;
